52c3e59ef3e05cf897dbedb648c1cf0cc70f80b3
Mar Klep <martin.klepek@stud.uni-due.de>
2019-12-09 02:42:02 +0100

    Update SecureContainer.java

--- Day4/SecureContainer.java
+++ Day4/SecureContainer.java
@@ -35,26 +35,27 @@ public static boolean isValidPW(int i){
     return valid;
 }
 
-public static boolean rule3(String s){
+public static boolean rule3(int i){
+    String s = Integer.toString(i);
     boolean valid = false;
-    for(int i = 0; i < 5; i++){
-        if(s.charAt(i) == s.charAt(i+1)){
+    if(hasSameNeighbor(s) == false || doesntDecrease(s) == false){
+        return false;
+    }
+    else{
+        for(int j = 0; j < 4; j++){
+            if(s.charAt(j) == s.charAt(j+1) && s.charAt(j) != s.charAt(j+2)){
+                return true;
+            }
+        }
+    if(!valid){
+        if(s.charAt(4) == s.charAt(5)){     // special case for index 4 because of j+2 check
             return true;
         }
     }
-    return valid;
-}
-
-public static boolean isValidPW2(int i){
-    String pw = Integer.toString(i);
-    boolean valid = true;
-    if(rule3(pw) == false || doesntDecrease(pw) == false){
-        valid = false;
     }
-
     return valid;
 }
-
+   
 
     public static void main(String[] args) {
         int start = 178416;
@@ -64,12 +65,22 @@ public static boolean isValidPW2(int i){
         for(int x = start; x <= end; x++){
             if(isValidPW(x)){
                 count++;
-                System.out.println(x);
+                //System.out.println(x);
+            }
+        }
+
+        int count2 = 0;
+        for(int y = start; y <= end; y++){
+            if(rule3(y)){
+                count2++;
+                //System.out.println(y);
             }
         }
         
         
         System.out.println("# Valid Passwords: " + count); // Answer is 1650
+        System.out.println();
+        System.out.println("# Valid Passwords part 2: " + count2);
 
     }
 
